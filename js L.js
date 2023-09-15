c0a2c79e0feddcae249a8346d2d2bcf486366f2e
sandhya <sandhyanathsharma@gmail.com>
2023-09-15 18:00:54 +0530

    new file created

---
+++ js L.js
@@ -0,0 +1,10 @@
+function show(name1="xyz")
+{
+   alert("Welcome"+name+"to my webpage")
+   console.log("Welcome"+name1+"to my webpage)
+}
+for(var x=1;x<=5;x++)
+{
+    p=prompt("Enter the name :")
+    show(p)
+}
